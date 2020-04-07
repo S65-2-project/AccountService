1d6b9bb87e67d44112d2ce3f18a6ee9fc5a290c6
Joeri Berman <joeriberman@hotmail.com>
2020-04-07 16:12:16 +0200

    S655-121: Added Repository, database connection. Started working on tasks to be implemented so I can work with the database later on.

---
+++ AccountService/Domain/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace AccountService.Domain
+{
+    public class Account
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
