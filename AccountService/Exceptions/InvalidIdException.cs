1ae4f8bcb0798f24cfcd162df9f99e7d4c00612d
WouterVan <wouter04316@gmail.com>
2020-05-12 14:09:54 +0200

    Feature/s65 150 (#9)
    * S65-150: Refactored AccountController and AccService. Added password Hashing
    
    * S65-150: Added Login feature, Added JWToken for login. Further Refactoring
    
    * S65-150: Changed ChangePassword method so it checks the original password.
    
    * S65-150: Changed Regex string Email check, added unit testing
    
    * S65-150: Added several Unit tests
    
    TODO: All methods from AccountService.
    
    * S65-150: All methods in accservice except for delete are tested
    
    * S65-150: Renamed Acc to Account
    
    * S65-150: Renamed Acc to Account Part 2
    
    * S65-150: Fixed update method repository
    
    * S65-150: Code review feedback
    
    * S65-150: Authorize check
    
    * s65-150: use authorization correctly, and update account
    
    * s65-150: Added custom exceptions, fixed bugs see Jira
    
    * S65-150: Changed tests to custom exceptions
    
    * S65-150: BugFix
    
    * S65-150: Fix tests
    
    * S65-150: Rename tests
    
    * S65-150: Removed RabbitMQ and nuget package
    
    * S65-150: Implemented changes based on review pull request
    
    Co-authored-by: Bstijn <baltessen.stijn@gmail.com>
    Co-authored-by: Davy de Haas <davy.dehaas98@gmail.com>

---
+++ AccountService/Exceptions/InvalidIdException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AccountService.Exceptions
+{
+    [Serializable]
+    public class InvalidIdException : Exception
+    {
+        public InvalidIdException() : base("This id is invalid.")
+        {
+            
+        }
+    }
+}
--- AccountService/Messaging/IMessageQueuePublisher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace AccountService.Messaging
-{
-    public interface IMessageQueuePublisher
-    {
-        Task PublishMessageAsync<T>(string routingKey, string messageType, T value);
-    }
-}
